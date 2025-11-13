17ea81c0684bf0041345b5709c55a29b761bbf29
chowdhury-yeamin <chowdhuryyeamin07@gmail.com>
2025-11-13 20:20:06 +0600

    Added Dort function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -28,13 +28,65 @@ async function run() {
     const transactionsCollection = db.collection("transactions");
 
     app.get("/transactions", async (req, res) => {
-      const result = await transactionsCollection.find();
-      const transactions = await result.toArray();
-      res.send(transactions);
+      try {
+        const { sortBy, order } = req.query;
+
+        let sortField = { date: -1 };
+
+        if (sortBy === "amount") {
+          sortField = { amount: order === "asc" ? 1 : -1 };
+        } else if (sortBy === "date") {
+          sortField = { date: order === "asc" ? 1 : -1 };
+        }
+
+        const result = await transactionsCollection.find().sort(sortField);
+        const transactions = await result.toArray();
+        res.send(transactions);
+      } catch (err) {
+        console.error("Error fetching transactions:", err);
+        res.status(500).send({ message: "Server error" });
+      }
     });
 
+    app.get("/transactions/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const transaction = await transactionsCollection.findOne(query);
+      res.send(transaction);
+    });
 
-    
+    app.post("/transactions", async (req, res) => {
+      const transaction = req.body;
+      const result = await transactionsCollection.insertOne(transaction);
+      res.send(result);
+    });
+
+    app.delete("/transactions/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await transactionsCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.put("/transactions/:id", async (req, res) => {
+      const id = req.params.id;
+      const transaction = req.body;
+      const query = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          name: transaction.name,
+          email: transaction.email,
+        },
+      };
+      const result = await transactionsCollection.updateOne(
+        query,
+        updateDoc,
+        options
+      );
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
